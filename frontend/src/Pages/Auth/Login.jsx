c840e5d4d20170c813866603fdc8c7806a179a64
dnyaneshwarithinkify <dnyaneshwari.zirpe.thinkify@gmail.com>
2026-01-09 21:12:54 +0000

    login page updates

--- frontend/src/Pages/Auth/Login.jsx
+++ frontend/src/Pages/Auth/Login.jsx
@@ -4,9 +4,12 @@ import React from 'react';
 
 const Login = () => {
   return (
-   <div className="body">
-
-</div>  );
+    <div className="body">
+        <div className="App">
+            <h1>Catering management</h1>
+        </div>
+    </div>  
+  );
 };
 
 export default Login;
