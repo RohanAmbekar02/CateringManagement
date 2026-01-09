76a69b8c1d62e3ed2ce064edbc717e8781e1743d
dnyaneshwarithinkify <dnyaneshwari.zirpe.thinkify@gmail.com>
2026-01-09 21:04:53 +0000

    Login page updated

--- frontend/src/Pages/Auth/Login.jsx
+++ frontend/src/Pages/Auth/Login.jsx
@@ -4,23 +4,9 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div className="login-page">
-      <h2>Catering Management Login</h2>
-      <form>
-        <div className="input-group">
-          <label>Username:</label>
-          <input type="text" placeholder="Enter your name" />
-        </div>
-        <br />
-        <div className="input-group">
-          <label>Password:</label>
-          <input type="password" placeholder="Enter password"/>
-        </div>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+   <div className="body">
+
+</div>  );
 };
 
 export default Login;
