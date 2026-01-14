9f0c56deea55f49c8867ce2c2acf9bd851826129
RohanAmbekar02 <rohanambekar121.com>
2026-01-14 18:46:32 +0530

    Update Folder Structure

---
+++ frontend/src/Pages/Item/add-item.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddItem() {
+  return (
+    <div>Add Item</div>
+  )
+}
+
+export default AddItem
---
+++ frontend/src/Pages/Item/item-details.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ItemDetails() {
+  return (
+    <div>Item Details</div>
+  )
+}
+
+export default ItemDetails
