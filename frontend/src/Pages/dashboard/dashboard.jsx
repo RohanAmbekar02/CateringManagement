f91f89bc9ebb523d6c5c4834667d285654787c8b
pratikshaudage2209 <pratiksha.udage22.thinkify@gmail.com>
2026-01-15 01:58:00 -0800

    Dashboard page

--- frontend/src/Pages/dashboard/dashboard.jsx
+++ frontend/src/Pages/dashboard/dashboard.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import "./dashboard.css";
 
-function Dashboard() {
+const DashboardCards = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="dash-container container">
+      <div className="dash-card blue shadow">
+        <div className="dash-icon">👥</div>
+        <div className="dash-content">
+          <p>Total Customers</p>
+          <h2>89</h2>
+        </div>
+      </div>
 
-export default Dashboard
+      <div className="dash-card violet shadow">
+         <div className="dash-icon">🛒</div>
+        <div className="dash-content">
+           <p>Total Items</p>
+          <h2>238</h2>
+         </div>
+      </div>
+
+      <div className="dash-card orange shadow">
+        <div className="dash-icon">📦</div>
+        <div className="dash-content">
+          <p>Total Orders</p>
+          <h2>19</h2>
+        
+        </div>
+      </div>
+
+      <div className="dash-card teal shadow">
+        <div className="dash-icon">💰</div>
+        <div className="dash-content">
+          <p>Total Cost</p>
+          <h2>₹42K</h2>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardCards;
